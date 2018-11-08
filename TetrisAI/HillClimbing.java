539061a11d5115c38b31d112492968465028af45
ewurman <ewurman@gmail.com>
2018-11-08 00:12:11 -0500

    Started a bit

--- TetrisAI/HillClimbing.java
+++ TetrisAI/HillClimbing.java
@@ -14,28 +14,28 @@ to play each game.
 import java.lang.Math;
 
 public class HillClimbing {
-	int exploration_constant = 3;
-	// better  as array of weights
-	
+	int exploration_constant = 3; //Maybe do a percentage of the currect score instead?
 	int n = 1000;
 
 	public void SoftStochasticSearch(int maxHeight, int heightRange, int holes, int roughness, int blockades){
 
+		int[] weights = [maxHeight, heightRange, holes, roughness, blockades];
 		for (int i; i<n; i++){
-			int current_score = ;//score of game with current
 
-			double increment = getRandomNumberInRange();
-			int sample = getRandomNumberInRange(0, 4);
+			int current_score = 0;//TODO score of game with current
+			
 
-			int[] weights = [maxHeight, heightRange, holes, roughness, blockades];
-			weights[sample] += increment;
+			double increment = Math.random() - 0.5; //range of [-0.5, 0.5)
+			int sample = (int)(Math.random() * weights.length);
+
+			weights[sample] += increment; //searching random direction a bit
 
 
 			double first; = //score of a game with weights from the array with changed weight
 			double second; = //score of a game with given weights
 			double third; = //score of a game with given weights
 
-			double average = ;// average of first, second, third
+			double average = (first + second + third) / 3;// average of first, second, third
 
 			if (average > (current_score - exploration_constant)){
 				maxHeight = weights[0];
